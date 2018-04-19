103e03b540e1bf69956765ccb8b5bbbb849e578a
PixelJanitor <derek.briggs@me.com>
2018-04-19 10:51:11 -0700

    yuck js charting

--- app/components/organizations/billing-section.js
+++ app/components/organizations/billing-section.js
@@ -3,12 +3,116 @@ import {inject as service} from '@ember/service';
 import Component from '@ember/component';
 import AdminMode from 'percy-web/lib/admin-mode';
 
+let monthStart = 1;
+let monthEnd = 30;
+let chartLabels = Array(monthEnd - monthStart + 1)
+  .fill()
+  .map(() => monthStart++);
+
+let barDataSet = Array.from({length: 30}, () => Math.floor(Math.random() * 100000));
+
+let setUnusedDays = function() {
+  let findLargestNumber = Math.max.apply(Math, barDataSet);
+  return 0.02 * findLargestNumber;
+};
+
 export default Component.extend({
   organization: null,
   classes: null,
 
   isSaving: null,
   isSaveSuccessful: null,
+
+  CHARTDATA: {
+    labels: chartLabels,
+    datasets: [
+      {
+        data: barDataSet.fill(setUnusedDays(), 18),
+        backgroundColor: [
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(158, 102, 191, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+          'rgba(240, 238, 241, 1.00)',
+        ],
+      },
+    ],
+  },
+
+  CHARTOPTIONS: {
+    layout: {
+      padding: {
+        left: 500,
+        right: 0,
+        top: 0,
+        bottom: 0,
+      },
+    },
+    tooltips: {
+      enabled: false,
+    },
+    labels: {
+      fontColor: 'black',
+      defaultFontFamily: 'proxima-nova',
+    },
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [
+        {
+          gridLines: {display: false},
+          ticks: {
+            fontSize: 12,
+            fontFamily: "'proxima-nova', sans-serif",
+            fontColor: '#CAC5CC',
+            fontStyle: '900',
+          },
+        },
+      ],
+      yAxes: [
+        {
+          gridLines: {
+            color: 'rgba(251, 250, 252, 1.00)',
+            zeroLineColor: 'rgba(231, 231, 232, 1.00)',
+          },
+          ticks: {
+            beginAtZero: true,
+            fontSize: 12,
+            fontFamily: "'proxima-nova', sans-serif",
+            fontColor: '#CAC5CC',
+            fontStyle: '900',
+          },
+        },
+      ],
+    },
+  },
 
   subscriptionData: service(),
   classNames: ['OrganizationsBillingSection'],
@@ -17,6 +121,7 @@ export default Component.extend({
     let isCustomer = this.get('organization.subscription.isCustomer');
     return isCustomer && AdminMode.isAdmin();
   }),
+
   actions: {
     changingSubscription(savingPromise) {
       this.set('isSaveSuccessful', null);
