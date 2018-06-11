c3a8992a97b5eb75c642fff9fed9a3523f752043
LC <lrc3233@gmail.com>
2018-06-11 11:01:44 -0700

    move logic to component

--- app/components/projects/browser-family-selector.js
+++ app/components/projects/browser-family-selector.js
@@ -1,10 +1,40 @@
 // import {computed} from '@ember/object';
-// import {alias, or} from '@ember/object/computed';
+import {readOnly, mapBy} from '@ember/object/computed';
 // import PollingMixin from 'percy-web/mixins/polling';
 // import {inject as service} from '@ember/service';
 import Component from '@ember/component';
 // import moment from 'moment';
 
 export default Component.extend({
-  actions: {},
+  project: null,
+  allBrowserFamilies: null,
+
+  projectBrowserTargets: readOnly('project.projectBrowserTargets'),
+  existingBrowserTargets: mapBy('projectBrowserTargets', 'browserTarget'),
+  numExistingBrowserTargets: readOnly('existingBrowserTargets.length'),
+
+  actions: {
+    updateProjectBrowserTargets(targetFamily) {
+      // {<str:familyId>: <Obj:browserTarget>}
+      const existingBrowserTargetsByFamilyId = this.get('existingBrowserTargets').reduce(
+        (acc, browserTarget) => {
+          acc[browserTarget.get('browserFamily.id')] = browserTarget;
+          return acc;
+        },
+        {},
+      );
+
+      const projectHasBrowserFamily = targetFamily.get('id') in existingBrowserTargetsByFamilyId;
+
+      if (projectHasBrowserFamily) {
+        if (this.get('numExistingBrowserTargets') === 1) {
+          this.get('flashMessages').info('A project must have at least one browser');
+          return;
+        }
+        this.removeProjectBrowserTargetForFamily(targetFamily, this.get('project'));
+      } else {
+        this.addProjectBrowserTargetForFamily(targetFamily, this.get('project'));
+      }
+    },
+  },
 });
--- app/routes/organization/project/settings.js
+++ app/routes/organization/project/settings.js
@@ -15,23 +15,6 @@ export default Route.extend(AuthenticatedRouteMixin, {
     return hash({organization, project, projects, browserFamilies});
   },
 
-  _removeProjectBrowserTargetForFamily(familyToRemove, project) {
-    const projectBrowserTargetForFamily = project.get('projectBrowserTargets').find(function(pbt) {
-      return pbt.get('browserTarget.browserFamily.id') === familyToRemove.get('id');
-    });
-
-    projectBrowserTargetForFamily.destroyRecord();
-  },
-
-  _addProjectBrowserTargetForFamily(familyToAdd, project) {
-    const newProjectBrowserTarget = this.get('store').createRecord('projectBrowserTarget', {
-      project,
-      _browserFamily: familyToAdd,
-    });
-
-    newProjectBrowserTarget.save();
-  },
-
   actions: {
     projectUpdated(project) {
       // If project slug changed, redirect to new URL slug:
@@ -40,30 +23,23 @@ export default Route.extend(AuthenticatedRouteMixin, {
       this.transitionTo('organization.project.index', organizationSlug, projectSlug);
     },
 
-    updateProjectBrowserTargets(targetFamily) {
-      const project = this.modelFor(this.routeName).project;
-      const projectBrowserTargetsForProject = project.get('projectBrowserTargets');
-      const existingBrowserTargets = projectBrowserTargetsForProject.mapBy('browserTarget');
+    removeProjectBrowserTargetForFamily(familyToRemove, project) {
+      const projectBrowserTargetForFamily = project
+        .get('projectBrowserTargets')
+        .find(function(pbt) {
+          return pbt.get('browserTarget.browserFamily.id') === familyToRemove.get('id');
+        });
 
-      const existingBrowserTargetsByFamilyId = existingBrowserTargets.reduce(
-        (acc, browserTarget) => {
-          acc[browserTarget.get('browserFamily.id')] = browserTarget;
-          return acc;
-        },
-        {},
-      );
+      projectBrowserTargetForFamily.destroyRecord();
+    },
 
-      const projectHasBrowserFamily = targetFamily.get('id') in existingBrowserTargetsByFamilyId;
+    addProjectBrowserTargetForFamily(familyToAdd, project) {
+      const newProjectBrowserTarget = this.get('store').createRecord('projectBrowserTarget', {
+        project,
+        _browserFamily: familyToAdd,
+      });
 
-      if (projectHasBrowserFamily) {
-        if (existingBrowserTargets.get('length') === 1) {
-          this.get('flashMessages').info('A project must have at least one browser');
-          return;
-        }
-        this._removeProjectBrowserTargetForFamily(targetFamily, project);
-      } else {
-        this._addProjectBrowserTargetForFamily(targetFamily, project);
-      }
+      newProjectBrowserTarget.save();
     },
   },
 });
