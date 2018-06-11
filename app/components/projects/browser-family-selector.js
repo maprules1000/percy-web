7460932773d75d1bfb8f2eae2327e96c10b756bd
LC <lrc3233@gmail.com>
2018-06-11 10:51:40 -0700

    move to component

---
+++ app/components/projects/browser-family-selector.js
@@ -0,0 +1,10 @@
+// import {computed} from '@ember/object';
+// import {alias, or} from '@ember/object/computed';
+// import PollingMixin from 'percy-web/mixins/polling';
+// import {inject as service} from '@ember/service';
+import Component from '@ember/component';
+// import moment from 'moment';
+
+export default Component.extend({
+  actions: {},
+});
