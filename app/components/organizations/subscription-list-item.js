2a1325b0e682960d40ab901a893c0d78f8669be0
LC <lrc3233@gmail.com>
2018-07-17 15:02:41 -0700

    allow changing subscription if already customer

--- app/components/organizations/subscription-list-item.js
+++ app/components/organizations/subscription-list-item.js
@@ -5,7 +5,17 @@ import {inject as service} from '@ember/service';
 export default Component.extend({
   tagName: '',
   subscriptionData: service(),
+  subscriptionService: service('subscriptions'),
   isCustom: computed(function() {
     return this.get('subscriptionData.PLAN_IDS').indexOf(this.get('planData.id')) === -1;
   }),
+
+  actions: {
+    updateExistingSubscription() {
+      this.get('subscriptionService').changeSubscription.perform(
+        this.get('organization'),
+        this.get('planData.id'),
+      );
+    },
+  },
 });
