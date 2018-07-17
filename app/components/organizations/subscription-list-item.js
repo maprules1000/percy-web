47581f38c640fbf05af0b6362a97f7ea95cf460c
LC <lrc3233@gmail.com>
2018-07-17 16:28:28 -0700

    wip

--- app/components/organizations/subscription-list-item.js
+++ app/components/organizations/subscription-list-item.js
@@ -4,18 +4,52 @@ import {inject as service} from '@ember/service';
 
 export default Component.extend({
   tagName: '',
+  existingPlan: null,
+  planData: null,
+  organization: null,
+
   subscriptionData: service(),
   subscriptionService: service('subscriptions'),
   isCustom: computed(function() {
     return this.get('subscriptionData.PLAN_IDS').indexOf(this.get('planData.id')) === -1;
   }),
 
+  isActivePlan: computed('existingPlan.id', 'planData.id', function() {
+    return this.get('existingPlan.id') === this.get('planData.id');
+  }),
+
+  subscriptionButtonText: computed('buttonText', 'isActivePlan', function() {
+    if (this.get('buttonText')) {
+      return this.get('buttonText');
+    } else if (this.get('isActivePlan')) {
+      return 'Selected Plan';
+    } else {
+      return 'Select Plan';
+    }
+  }),
+
+  isShowingCardUpdater: false,
+
+  showCardUpdater() {
+    this.set('isShowingCardUpdater', true);
+  },
+
+  updateExistingSubscription() {
+    this.get('subscriptionService').changeSubscription.perform(
+      this.get('organization'),
+      this.get('planData.id'),
+    );
+  },
+
   actions: {
-    updateExistingSubscription() {
-      this.get('subscriptionService').changeSubscription.perform(
-        this.get('organization'),
-        this.get('planData.id'),
-      );
+    handleSubscriptionSelection() {
+      if (this.get('isCustom')) {
+        return this.get('handleSelection')();
+        // } else if (this.get('organization.subscription.isCustomer')) {
+        //   return this.updateExistingSubscription();
+      } else {
+        this.showCardUpdater();
+      }
     },
   },
 });
